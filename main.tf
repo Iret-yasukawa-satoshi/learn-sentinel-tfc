cf494c1d3f0fd53b0fcb6a9465d6f0276fbda666
Iret-yasukawa-satoshi <106936301+Iret-yasukawa-satoshi@users.noreply.github.com>
2022-06-13 05:04:42 +0900

    Fix data resource ami

--- main.tf
+++ main.tf
@@ -2,24 +2,13 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+data "aws_ssm_parameter" "amzn2_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami                    = data.aws_ami.ubuntu.id
+
+resource "aws_instance" "amazonlinux2" {
+  ami                    = data.aws_ssm_parameter.amzn2_ami.id
   instance_type          = var.instance_type
 
   tags = {
