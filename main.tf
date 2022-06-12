880ae4c72fbad74005aec2d159911822b4672da6
Iret-yasukawa-satoshi <106936301+Iret-yasukawa-satoshi@users.noreply.github.com>
2022-06-13 05:18:00 +0900

    Add AMI

--- main.tf
+++ main.tf
@@ -2,19 +2,19 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "amzn2_ami" {
-  most_recent = true
+# data "aws_ami" "amzn2_ami" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20220426.0-x86_64-gp2"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-2.0.20220426.0-x86_64-gp2"]
+#   }
 
-  owners = ["137112412989"] # Canonical
-}
+#   owners = ["137112412989"] # Canonical
+# }
 
 resource "aws_instance" "amazonlinux2" {
-  ami                    = data.aws_ami.amzn2_ami.id
+  ami                    = "ami-0f9a314ce79311c88"
   instance_type          = var.instance_type
 
   tags = {
